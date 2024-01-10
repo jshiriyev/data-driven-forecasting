50867fec650499095dec2ba66e70583a38e5610a
Javid Shiriyev <jshiriyev@utexas.edu>
2024-01-10 22:18:29 +0400

    first notebook exercise, very helpful

--- IPRcurve.py
+++ IPRcurve.py
@@ -8,7 +8,7 @@ class IPR():
         for key,value in kwargs.items():
             setattr(self,key,value)
 
-    def pi_transient(self,time=1):
+    def PItransient(self,time=1):
         """time in days"""
 
         upper = (self.perm*self.height)
@@ -19,7 +19,7 @@ class IPR():
 
         return upper/lower
 
-    def pi_steady(self):
+    def PIsteady(self):
 
         upper = (self.perm*self.height)
 
@@ -27,7 +27,7 @@ class IPR():
 
         return upper/lower
 
-    def pi_pseudo(self):
+    def PIpseudo(self):
 
         upper = (self.perm*self.height)
 
@@ -35,18 +35,18 @@ class IPR():
 
         return upper/lower
 
-    def undersaturated(self,pres,rate=None,pwf=None,state="steady",**kwargs):
+    def undersaturated(self,pres,rate=None,pwf=None,regime="pseudo",**kwargs):
         
-        productivity = getattr(self,f"pi_{state}")(**kwargs)
+        productivity = getattr(self,f"PI{regime}")(**kwargs)
 
         if rate is None:
             return productivity*(pres-pwf)
         
         return pres-rate/productivity
 
-    def vogel(self,pres,rate=None,pwf=None,state="steady",**kwargs):
+    def vogel(self,pres,rate=None,pwf=None,regime="pseudo",**kwargs):
 
-        qmax = getattr(self,f"pi_{state}")(**kwargs)*pres/1.8
+        qmax = getattr(self,f"PI{regime}")(**kwargs)*pres/1.8
 
         if rate is None:
             return qmax*(1-0.2*(pwf/pres)-0.8*(pwf/pres)**2)
@@ -57,9 +57,9 @@ class IPR():
         
         return 0.125*pres*(numpy.sqrt(values)-1)
 
-    def fetkovich(self,pres,rate=None,pwf=None,n=None,state="steady",**kwargs):
+    def fetkovich(self,pres,rate=None,pwf=None,n=None,regime="pseudo",**kwargs):
 
-        qmax = getattr(self,f"pi_{state}")(**kwargs)*pres/1.8
+        qmax = getattr(self,f"PI{regime}")(**kwargs)*pres/1.8
 
         if rate is None:
             return qmax*(1-(pwf/pres)**2)**n
@@ -70,13 +70,17 @@ class IPR():
 
         return pres*numpy.sqrt(values)
 
-    def saturated(self,pres,pwf=None,state="steady",**kwargs): #vogel
+    def saturated(self,pres,rate=None,pwf=None,model="vogel",n=None,regime="pseudo",**kwargs):
+
+        return
+
+    def partial(self,pres,pbubble,rate=None,pwf=None,model="vogel",n=None,regime="pseudo",**kwargs):
 
         rate = numpy.zeros(pwf.shape)
 
         rate[pwf<0] = numpy.nan
 
-        PI = getattr(self,f"pi_{state}")(**kwargs)
+        PI = getattr(self,f"PI{regime}")(**kwargs)
 
         rate[pwf>self.pb] = PI*(pres-pwf[pwf>self.pb])
 
@@ -94,7 +98,6 @@ if __name__ == "__main__":
     perm = 8.2      # mD
     height = 53     # ft
     Pres = 5651     # psi
-    Pb = 3000    # psi
     Bo = 1.1
     muo = 1.7       # cp
     ct = 1.29e-5    # 1/psi
@@ -104,11 +107,12 @@ if __name__ == "__main__":
 
     re = numpy.sqrt((43560*darea)/numpy.pi)
 
+    Pb = 3000    # psi
+
     inflow = IPR(
         poro = poro,
         perm = perm,
         height = height,
-        pb = Pb,
         Bo = Bo,
         muo = muo,
         ct = ct,
@@ -122,25 +126,25 @@ if __name__ == "__main__":
     # prange = numpy.array((5651,5000,4500,4000,3500,3000,2500,2000,1500,1000,500,0))
     # prange = numpy.array((0,565,1130,1695,2260,2826,3000,5651))
 
-    p_transt = inflow.undersaturated(Pres,rate=qrange,state="transient",time=30)
-    p_steady = inflow.undersaturated(Pres,rate=qrange,state="steady")
-    p_pseudo = inflow.undersaturated(Pres,rate=qrange,state="pseudo")
+    p_transt = inflow.undersaturated(Pres,rate=qrange,regime="transient",time=30)
+    p_steady = inflow.undersaturated(Pres,rate=qrange,regime="steady")
+    p_pseudo = inflow.undersaturated(Pres,rate=qrange,regime="pseudo")
 
-    q_transt = inflow.undersaturated(Pres,pwf=prange,state="transient",time=30)
-    q_steady = inflow.undersaturated(Pres,pwf=prange,state="steady")
-    q_pseudo = inflow.undersaturated(Pres,pwf=prange,state="pseudo")
+    q_transt = inflow.undersaturated(Pres,pwf=prange,regime="transient",time=30)
+    q_steady = inflow.undersaturated(Pres,pwf=prange,regime="steady")
+    q_pseudo = inflow.undersaturated(Pres,pwf=prange,regime="pseudo")
     
-    # p_vogel = inflow.vogel(Pres,rate=qrange,state="transient",time=30)
-    # p_vogel = inflow.vogel(Pres,rate=qrange,state="steady")
-    p_vogel = inflow.vogel(Pres,rate=qrange,state="pseudo")
+    # p_vogel = inflow.vogel(Pres,rate=qrange,regime="transient",time=30)
+    # p_vogel = inflow.vogel(Pres,rate=qrange,regime="steady")
+    p_vogel = inflow.vogel(Pres,rate=qrange,regime="pseudo")
 
-    # q_vogel = inflow.vogel(Pres,pwf=prange,state="transient",time=30)
-    # q_vogel = inflow.vogel(Pres,pwf=prange,state="steady")
-    q_vogel1 = inflow.vogel(Pres,pwf=prange,state="pseudo")
+    # q_vogel = inflow.vogel(Pres,pwf=prange,regime="transient",time=30)
+    # q_vogel = inflow.vogel(Pres,pwf=prange,regime="steady")
+    q_vogel1 = inflow.vogel(Pres,pwf=prange,regime="pseudo")
 
-    q_fetkovich = inflow.fetkovich(Pres,pwf=prange,state="pseudo",n=2)
+    q_fetkovich = inflow.fetkovich(Pres,pwf=prange,regime="pseudo",n=2)
 
-    q_vogel2 = inflow.saturated(Pres,pwf=prange,state="pseudo")
+    q_vogel2 = inflow.saturated(Pres,pwf=prange,regime="pseudo")
 
     # plt.plot(qrange,pwf_transt,label='Transient')
     # plt.plot(qrange,pwf_steady,label='Steady-State')
@@ -148,7 +152,7 @@ if __name__ == "__main__":
 
     # plt.plot(q_pseudo,prange,label='Pseudo')
     plt.plot(q_vogel1,prange,label='Vogel')
-    plt.plot(q_vogel2,prange,label='3000')
+    # plt.plot(q_vogel2,prange,label='3000')
     # plt.plot(q_fetkovich,prange,label='Fetkovich')
 
     plt.legend()
