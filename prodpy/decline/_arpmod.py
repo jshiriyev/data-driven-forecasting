03c87bc2773a3b4431b56eaeb7b3b13a6a294092
Javid Shiriyev, Ph.D. <jshiriyev@utexas.edu>
2024-09-29 23:09:14 +0400

    inversion works perfectly fine

--- prodpy/decline/_arpmod.py
+++ prodpy/decline/_arpmod.py
@@ -4,6 +4,8 @@ import numpy
 
 from scipy._lib._bunch import _make_tuple_bunch
 
+from scipy.optimize import curve_fit
+
 from scipy.stats import linregress
 from scipy.stats import norm
 from scipy.stats import t as tstat
@@ -59,142 +61,119 @@ class Arps:
 
 		return self.option(mode=None,b=b)
 
-	def frw(self,Di:float,yi:float,x:numpy.ndarray,xi:float=0.):
+	def frw(self,x:numpy.ndarray,Di:float,yi:float,*,xi:float=0.):
 		"""Returns the result of forward calculations."""
-		inp = (Di,yi,x,xi) if self.b==0. or self.b==1. else (Di,yi,x,xi,self.b)
-		return getattr(self,f"frw{self.mode}")(*inp)
+		kwargs = dict(xi=xi) if self.b==0. or self.b==1. else dict(xi=xi,b=self.b)
+		return getattr(self,f"frw{self.mode}")(x,Di,yi,**kwargs)
 
 	@staticmethod
-	def frwexp(Di:float,yi:float,x:numpy.ndarray,xi:float=0.):
+	def frwexp(x:numpy.ndarray,Di:float,yi:float,*,xi:float=0.):
 		"""
 		q = qi * exp(-Di*t)
 		"""
 		return yi*numpy.exp(-Di*(numpy.asarray(x)-xi))
 
 	@staticmethod
-	def frwhyp(Di:float,yi:float,x:numpy.ndarray,xi:float=0.,b:float=0.5):
+	def frwhyp(x:numpy.ndarray,Di:float,yi:float,*,xi:float=0.,b:float=0.5):
 		"""
 		q = q0 / (1+b*Di*t)**(1/b)
 		"""
 		return yi/(1+b*Di*(numpy.asarray(x)-xi))**(1./b)
 
 	@staticmethod
-	def frwhar(Di:float,yi:float,x:numpy.ndarray,xi:float=0.):
+	def frwhar(x:numpy.ndarray,Di:float,yi:float,*,xi:float=0.):
 		"""
 		q = q0 / (1+Di*t)
 		"""
 		return yi/(1+Di*(numpy.asarray(x)-xi))
 
-	def cum(self,Di:float,yi:float,x:numpy.ndarray,xi:float=0.):
+	def cum(self,x:numpy.ndarray,Di:float,yi:float,*,xi:float=0.):
 		"""Returns the result of cumulative calculations."""
 		inp = (Di,yi,x,xi) if self.b==0. or self.b==1. else (Di,yi,x,xi,self.b)
 		return getattr(self,f"cum{self.mode}")(*inp)
 
 	@staticmethod
-	def cumexp(Di:float,yi:float,x:numpy.ndarray,xi:float=0.):
+	def cumexp(x:numpy.ndarray,Di:float,yi:float,*,xi:float=0.):
 		"""
 		Np = qi / Di * (1-exp(-Di*t))
 		"""
 		return (yi/Di)*(1-numpy.exp(-Di*(numpy.asarray(x)-xi)))
 
 	@staticmethod
-	def cumhyp(Di:float,yi:float,x:numpy.ndarray,xi:float=0.,b:float=0.5):
+	def cumhyp(x:numpy.ndarray,Di:float,yi:float,*,xi:float=0.,b:float=0.5):
 		"""
 		Np = q0 / ((1-b)*Di)*(1-(1+b*Di*t)**(1-1/b))
 		"""
 		return (yi/Di)/(1-b)*(1-(1+b*Di*(numpy.asarray(x)-xi))**(1-1./b))
 
 	@staticmethod
-	def cumhar(Di:float,yi:float,x:numpy.ndarray,xi:float=0.):
+	def cumhar(x:numpy.ndarray,Di:float,yi:float,*,xi:float=0.):
 		"""
 		Np = q0 / Di * ln(1+Di*t)
 		"""
 		return (yi/Di)*numpy.log(1+Di*(numpy.asarray(x)-xi))
 
-	def inv(self,x:numpy.ndarray,y:numpy.ndarray,xi:float=None):
+	def inv(self,x:numpy.ndarray,y:numpy.ndarray,*,xi:float=0.):
 		"""Returns regression results after linearization."""
-		inp = (x,y,xi) if self.b==0. or self.b==1. else (x,y,xi,self.b)
-		return getattr(self,f"inv{self.mode}")(*inp)
+		kwargs = dict(xi=xi) if self.b==0. or self.b==1. else dict(xi=xi,b=self.b)
+		return getattr(self,f"inv{self.mode}")(x,y,**kwargs)
 
 	@staticmethod
-	def invexp(x:numpy.ndarray,y:numpy.ndarray,xi:float=None):
+	def invexp(x:numpy.ndarray,y:numpy.ndarray,*,xi:float=0.):
 		"""Returns exponential regression results after linearization."""
 
-		x,y = Arps.shift(x,y,xi)
+		x,y = Arps.shift(numpy.asarray(x),numpy.asarray(y),xi)
 		x,y = Arps.nzero(x,y)
-
-		n,df = x.size,x.size-2
 
 		linear = Arps.regress(x,numpy.log(y))
 
-		Sxx = numpy.nansum((x-np.nanmean(x))**2)
+		linfit = -linear.slope,numpy.exp(linear.intercept)
 
-		Di,yi = -linear.slope,numpy.exp(linear.intercept)
+		result = curve_fit(Arps.frwexp,x,y,p0=linfit)
 
-		ycal = Arps.frwexp(Di,yi,x)
+		R2 = Arps.rsquared(Arps.frwexp(x,*result[0]),y)
 
-		R2 = Arps.rsquared(ycal,y)
+		perror = numpy.sqrt(numpy.diag(result[1]))
 
-		s2 = numpy.sum((ycal-y)**2)/df
-
-		Di_error = numpy.sqrt(s2/Sxx)
-		yi_error = numpy.sqrt(s2/n/Sxx*numpy.nansum(x**2))
-
-		return Result(0.,Di,yi,xi,n,R2,Di_error,yi_error,linear)
+		return Result(0.,*result[0],xi,x.size,R2,*perror,linear)
 
 	@staticmethod
-	def invhyp(x:numpy.ndarray,y:numpy.ndarray,xi:float=None,b:float=0.5):
+	def invhyp(x:numpy.ndarray,y:numpy.ndarray,*,xi:float=0.,b:float=0.5):
 		"""Returns hyperbolic regression results after linearization."""
 
-		x,y = Arps.shift(x,y,xi)
+		x,y = Arps.shift(numpy.asarray(x),numpy.asarray(y),xi)
 		x,y = Arps.nzero(x,y)
-
-		n,df = x.size,x.size-2
 
 		linear = Arps.regress(x,numpy.power(1/y,b))
 
-		Sxx = numpy.nansum((x-np.nanmean(x))**2)
+		linfit = linear.slope/linear.intercept/b,linear.intercept**(-1/b)
 
-		Di,yi = linear.slope/linear.intercept/b,linear.intercept**(-1/b)
+		result = curve_fit(Arps.frwhyp,x,y,p0=linfit)
 
-		ycal = Arps.frwhyp(Di,yi,x,b=b)
+		R2 = Arps.rsquared(Arps.frwhyp(x,*result[0],b=b),y)
 
-		R2 = Arps.rsquared(ycal,y)
+		perror = numpy.sqrt(numpy.diag(result[1]))
 
-		s2 = numpy.sum((ycal-y)**2)/df
-
-		R2 = Hyperbolic(*params,self.b).rsquared(x,yobs)
-
-		Di_error = numpy.sqrt(s2/Sxx)
-		yi_error = numpy.sqrt(s2/n/Sxx*numpy.nansum(x**2))
-
-		return Result(b,Di,yi,xi,n,R2,Di_error,yi_error,linear)
+		return Result(b,*result[0],xi,x.size,R2,*perror,linear)
 
 	@staticmethod
-	def invhar(x:numpy.ndarray,y:numpy.ndarray,xi:float=None):
+	def invhar(x:numpy.ndarray,y:numpy.ndarray,*,xi:float=0.):
 		"""Returns harmonic regression results after linearization."""
 
-		x,y = Arps.shift(x,y,xi)
+		x,y = Arps.shift(numpy.asarray(x),numpy.asarray(y),xi)
 		x,y = Arps.nzero(x,y)
-
-		n,df = x.size,x.size-2
 
 		linear = Arps.regress(x,1./y)
 
-		Sxx = numpy.nansum((x-np.nanmean(x))**2)
+		linfit = linear.slope/linear.intercept,1/linear.intercept
 
-		Di,yi = linear.slope/linear.intercept,1/linear.intercept
+		result = curve_fit(Arps.frwhar,x,y,p0=linfit)
 
-		ycal = Arps.frwhar(Di,yi,x)
+		R2 = Arps.rsquared(Arps.frwhar(x,*result[0]),y)
 
-		R2 = Arps.rsquared(ycal,y)
+		perror = numpy.sqrt(numpy.diag(result[1]))
 
-		s2 = numpy.sum((ycal-y)**2)/df
-
-		Di_error = numpy.sqrt(s2/Sxx)
-		yi_error = numpy.sqrt(s2/n/Sxx*numpy.nansum(x**2))
-
-		return Result(1.,Di,yi,xi,n,R2,Di_error,yi_error,linear)
+		return Result(1.,*result[0],xi,x.size,R2,*perror,linear)
 
 	@staticmethod
 	def get_mode(b:float):
@@ -252,11 +231,11 @@ class Arps:
 		return 1-ssres/sstot
 
 	@staticmethod
-	def model(result:Result,perc:float=0.5):
-		"""perc -> percentile, perc=0.5 gives mean values"""
+	def model(result:Result,prc:float=50.):
+		"""prc -> prcentile, prc=0.5 gives mean values"""
 
-		Di = result.Di+tstat.ppf(prc,result.df)*result.Di_error
-		yi = result.yi+tstat.ppf(prc,result.df)*result.yi_error
+		Di = result.Di+tstat.ppf(prc/100.,result.n-2)*result.Di_error
+		yi = result.yi-tstat.ppf(prc/100.,result.n-2)*result.yi_error
 
 		return Di,yi
 
@@ -264,20 +243,27 @@ if __name__ == "__main__":
 
 	import matplotlib.pyplot as plt
 
-	x = numpy.linspace(0,500,5000)
+	x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15.]
+	y = [1000,963,927,897,852,826,792,750,727,693,659,646,618,588,567,541]
 
-	y1 = Arps(0).frw(0.005,3,x,xi=50)
-	y2 = Arps(0.5).frw(0.005,3,x,xi=50)
-	y3 = Arps(1).frw(0.005,3,x,xi=50)
+	plt.scatter(x,y)
 
-	# c1 = Arps(0).cum(0.005,3,x)
-	# c2 = Arps(0.5).cum(0.005,3,x)
-	# c3 = Arps(1).cum(0.005,3,x)
+	result = Arps(1.).inv(x,y)
 
-	plt.plot(x,y1,label='Exponential')
-	plt.plot(x,y2,label='Hyperbolic')
-	plt.plot(x,y3,label='Harmonic')
+	print(result)
 
+	p10 = Arps.model(result,prc=10.)
+	p50 = Arps.model(result,prc=50.)
+	p90 = Arps.model(result,prc=90.)
+
+	fit10 = Arps(1.).frw(x,*p10)
+	fit50 = Arps(1.).frw(x,*p50)
+	fit90 = Arps(1.).frw(x,*p90)
+
+	plt.plot(x,fit10,label='p10')
+	plt.plot(x,fit50,label='p50')
+	plt.plot(x,fit90,label='p90')
+	
 	plt.legend()
 
 	plt.show()
