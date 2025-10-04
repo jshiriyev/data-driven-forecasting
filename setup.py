38fab4260d91afbf976755658dd6ced48bc9f070
Javid Shiriyev, Ph.D. <jshiriyev@utexas.edu>
2025-10-04 23:34:25 +0400

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'prodpy',
-	version = '0.0.30',
+	version = '0.0.32',
 	packages = find_packages(),
 	install_requires = [
 		'numpy>=2.3.1',
